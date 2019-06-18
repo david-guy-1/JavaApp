7c24174a7f44a424796c220df484b2299406213a
david <jannaultheal@gmail.com>
2019-06-18 12:03:54 -0400

    jddfkdjo;ipeokfdjoipekwsmjolmlfjoelmlefj

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		for(int i=0;i<10;i++){
+			System.out.println("i is now " + i);
+			
+		}
 	}
 
 }
